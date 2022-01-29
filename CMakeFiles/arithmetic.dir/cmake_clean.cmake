d935f87c47d25b171e3e3932387c788d983856aa
Jenkins <jenkins@krishna.us-west1-b.c.rapid-stream-338014.internal>
2022-01-29 10:28:04 +0000

    22

---
+++ CMakeFiles/arithmetic.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/arithmetic.dir/main.cpp.o"
+  "CMakeFiles/arithmetic.dir/addition.cpp.o"
+  "CMakeFiles/arithmetic.dir/division.cpp.o"
+  "CMakeFiles/arithmetic.dir/print_result.cpp.o"
+  "arithmetic.pdb"
+  "arithmetic"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/arithmetic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
